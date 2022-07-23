115760a3db3f4af8c943ada46ad72097b6a6d709
mjd10m <mjd10m@outlook.com>
2022-07-23 17:08:57 -0400

    bac calc w time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 
 var drinks = []
-var drink1 = {abv: 0.045, ounces: 12}
-var drink2 = {abv: 0.045, ounces: 12}
+var drink1 = {abv: 0.05, ounces: 12}
+var drink2 = {abv: 0.05, ounces: 12}
 var weightInPounds = 127
 var totalTimeInHours = 2
 var gender = "male"
@@ -9,17 +9,17 @@ var metabolism = 0.012
 
 drinks.push(drink1)
 drinks.push(drink2)
-
+var timestamp = Date.now()
 function calculateBAC() {
     if (gender == "male"){
         var alcoholGrams = convertDrinkToGrams(drinks)
-        debugger;
-        bac = (alcoholGrams/((weightInPounds * 454) * .68)) * 100
+        bac = ((alcoholGrams/((weightInPounds * 454) * .68)) * 100) - (totalTimeInHours * .015) 
     } else {
         convertDrinkToGrams(drinks)
-        bac = (alcoholGrams/((weightInPounds * 454) * .55)) * 100
+        bac = ((alcoholGrams/((weightInPounds * 454) * .55)) * 100) - (totalTimeInHours * .015) 
     }
-    console.log(bac)
+    roundedBac = Math.round(bac * 1000) / 1000
+    console.log(roundedBac)
 }
 
 function convertDrinkToGrams(drinks) {
